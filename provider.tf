61b8d142d255e8f78e0aacbef58c2c2bfb844142
GVindio <97354117+GVindio@users.noreply.github.com>
2022-11-06 17:00:56 -0500

    second commit

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@
 resource "aws_instance" "nodeDeploy" {
     ami = "ami-026b57f3c383c2eec"
     count = "1"
-    key_name   = "Jenkins"
+    key_name   = "testlab"
     instance_type = "t2.micro"
     security_groups = ["launch-wizard-1"]
     tags = {
