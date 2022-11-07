87f15e2e6a153a65d6d930c3d258d1e55c3c21ca
GVindio <97354117+GVindio@users.noreply.github.com>
2022-11-06 19:57:38 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,14 +5,14 @@
 
 
 
-resource "aws_instance" "nodeDeploy" {
+resource "aws_instance" "nodejs" {
     ami = "ami-026b57f3c383c2eec"
     count = "1"
     key_name   = "testlab"
     instance_type = "t2.micro"
     security_groups = ["launch-wizard-1"]
     tags = {
-        Name = "nodedeploy"
+        Name = "NodeJS"
     } 
     user_data = file("node.sh")
    
